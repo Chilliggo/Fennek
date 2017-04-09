f0bc3dbe9e13e404efd23870694419ad19787b1b
Nico H <nico.himmelein@web.de>
2017-04-09 14:51:42 +0200

    adding onepage-scroll to index

--- Controller/main.js
+++ Controller/main.js
@@ -1,1 +1,27 @@
-!function(t){"use strict";var a=t("[data-slides]"),ni=0,s=a.data("slides"),e=s.length,n=function(){if(ni>=a.length){ni=0}else{ni++};a.css("background-image",'url("'+s[ni]+'")').show(0,function(){setTimeout(n,5e3)})};n()}(jQuery);
+! function ($) {
+    "use strict";
+    var slide = $("[data-slides]"),
+        count = 0,
+        slides = slide.data("slides"),
+        len = slides.length,
+        n = function () {
+            if (count >= len) {
+                count = 0
+            }
+            slide.css("background-image", 'url("' + slides[count] + '")').show(0, function () {
+                setTimeout(n, 5e3)
+            });
+            count++;
+        };
+    n()
+}(jQuery);
+
+$(document).ready(function () {
+    $(".main").onepage_scroll({
+        sectionContainer: "section",
+        easing: "ease",
+        animationTime: 500,
+        pagination: true,
+        updateURL: false
+    });
+});
